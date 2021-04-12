6b088d0d141518c7f6d1f7daa9f1fcae59f51643
abeidahmed <abeidahmed92@gmail.com>
2021-04-12 12:12:49 +0530

    Fix: Go to session path when using capybara

--- spec/support/features/sessions_helper.rb
+++ spec/support/features/sessions_helper.rb
@@ -2,7 +2,7 @@ module Features
   module SessionsHelper
     def feature_sign_in(user = nil)
       user ||= create(:user)
-      visit root_path
+      visit new_session_path
       fill_in "username", with: user.username
       fill_in "password", with: user.password
       click_button "Sign"
