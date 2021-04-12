9ed80c39c5563ba13c52dfb8a8b78b3d1a09b1c7
abeidahmed <abeidahmed92@gmail.com>
2021-04-12 15:33:16 +0530

    Add articles_controller create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,20 @@
 class ArticlesController < ApplicationController
   def new; end
+
+  def create
+    project = Project.find(params[:project_id])
+    article = project.articles.build(article_params.merge({ user: Current.user }))
+
+    if article.save
+      # do
+    else
+      render json: { errors: article.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title)
+  end
 end
--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,7 +1,14 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Articles", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "#create" do
+    it "creates a new article if the request is valid" do
+      user = create(:user)
+      project = create(:project, user: user)
+      sign_in(user)
+      post project_articles_path(project), params: { article: { title: "Foo bar article" } }
+
+      expect(project.articles.count).to eq(1)
+    end
   end
 end
