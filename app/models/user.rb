f2361187aaad80d318ae0d8d16b80711da255a44
abeidahmed <abeidahmed92@gmail.com>
2021-04-12 11:06:03 +0530

    Add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  VALID_USERNAME_REGEX = /\A^[a-zA-Z0-9]+\z/
+
+  validates :username, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }, format: { with: VALID_USERNAME_REGEX } # rubocop:disable Layout/LineLength
+  validates :password, presence: true, length: { minimum: 6 }
+
+  before_validation :strip_username
+  before_save :lowercase_username
+
+  private
+
+  def strip_username
+    self.username = username.to_s.strip
+  end
+
+  def lowercase_username
+    self.username = username.to_s.downcase
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { build(:user) }
+
+  describe "validations" do
+    it { is_expected.to have_secure_password }
+
+    it { is_expected.to validate_presence_of(:username) }
+
+    it { is_expected.to validate_uniqueness_of(:username).case_insensitive }
+
+    it { is_expected.to validate_length_of(:username).is_at_most(255) }
+
+    it { is_expected.to allow_value("johndoe", "john123").for(:username) }
+
+    it { is_expected.not_to allow_value("foo bar", "!foobar").for(:username) }
+
+    it { is_expected.to validate_length_of(:password).is_at_least(6) }
+  end
+
+  describe "callbacks" do
+    it "lowercases username before saving" do
+      username = "hello"
+      user.username = username.upcase
+      user.save!
+
+      expect(user.username).to eq(username)
+    end
+
+    it "strips the email before validation" do
+      username = "   hello  "
+      user.username = username
+      user.save!
+
+      expect(user.username).to eq("hello")
+    end
+  end
 end
