5703d2dbb98264e1a6f6d830cac313ec5b1c7cbc
abeidahmed <abeidahmed92@gmail.com>
2021-04-12 12:12:17 +0530

    Require authentication to use app

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -3,9 +3,14 @@ module Authenticable
 
   included do
     before_action :set_current_user
+    before_action :authenticate_user
   end
 
   def set_current_user
     Current.user = User.find_by(auth_token: cookies.signed[:auth_token]) if cookies[:auth_token]
+  end
+
+  def authenticate_user
+    redirect_to new_session_path, alert: "Please sign in or sign up" unless Current.user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: "sessions#new"
 
   resources :users, only: %i[create]
-  resources :sessions, only: %i[create]
+  resources :sessions, only: %i[new create]
 end
