b93d401bb79cf43254e5ae2d4be3a112519c3ed1
abeidahmed <abeidahmed92@gmail.com>
2021-04-12 14:09:15 +0530

    Enable focus-within

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,10 @@ module.exports = {
       },
     },
   },
-  variants: {},
+  variants: {
+    extend: {
+      boxShadow: ["focus-within"],
+    },
+  },
   plugins: [require("@tailwindcss/forms")],
 };
