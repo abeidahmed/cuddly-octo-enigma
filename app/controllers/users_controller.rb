84cbb785e38e8911dce19c3d65414507977f81a4
abeidahmed <abeidahmed92@gmail.com>
2021-04-12 12:29:11 +0530

    Add users sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user
 
+  def new
+    @user = User.new
+  end
+
   def create
     user = User.new(user_params)
 
     if user.save
       sign_in(user)
-      redirect_to root_path
+      redirect_to finders_path
     else
       render json: { errors: user.errors }, status: :unprocessable_entity
     end
---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe "SignUps", type: :feature do
+  it "after signing-up it redirects the user to the finders page" do
+    visit new_user_path
+    fill_in "Username", with: "johndoe"
+    fill_in "Password", with: "helloworld"
+    click_button "Create"
+
+    expect(page).to have_current_path(finders_path)
+  end
+end
