913a2f302a0c9543e6b273f64ece8b122954ca7f
abeidahmed <abeidahmed92@gmail.com>
2021-04-12 15:01:03 +0530

    Add project model validations

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Project, type: :model do
   describe "associations" do
     it { is_expected.to belong_to(:user) }
   end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+
+    it { is_expected.to validate_length_of(:name).is_at_most(255) }
+  end
 end
