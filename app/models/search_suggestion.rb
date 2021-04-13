285511ca39cf5381a32cd7134f13d2fcfd610a8e
abeidahmed <abeidahmed92@gmail.com>
2021-04-13 10:19:33 +0530

    Add search_suggestion model validations

--- app/models/search_suggestion.rb
+++ app/models/search_suggestion.rb
@@ -1,3 +1,5 @@
 class SearchSuggestion < ApplicationRecord
   belongs_to :project
+
+  validates :term, presence: true, uniqueness: { scope: :project_id, case_sensitive: false }
 end
--- spec/models/search_suggestion_spec.rb
+++ spec/models/search_suggestion_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe SearchSuggestion, type: :model do
   describe "associations" do
     it { is_expected.to belong_to(:project) }
   end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:term) }
+
+    it { is_expected.to validate_uniqueness_of(:term).scoped_to(:project_id).case_insensitive }
+  end
 end
