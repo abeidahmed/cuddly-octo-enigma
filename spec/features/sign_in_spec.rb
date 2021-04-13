6be488960f2db6d06b2ac15766f58ab8ffbc7f99
abeidahmed <abeidahmed92@gmail.com>
2021-04-13 13:53:55 +0530

    Redirect to root_path after logging in

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 RSpec.describe "SignIns", type: :feature do
-  it "redirects the user to project new page after successful sign in" do
+  it "redirects the user to root_path after successful sign in" do
     feature_sign_in
-    expect(page).to have_current_path(new_project_path)
+    expect(page).to have_current_path(root_path)
   end
 end
