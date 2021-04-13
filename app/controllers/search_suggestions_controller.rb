4d3e834bba5dcf7c2feaeefa1d0259473e043328
abeidahmed <abeidahmed92@gmail.com>
2021-04-13 22:16:51 +0530

    Order suggestions by created at

--- app/controllers/search_suggestions_controller.rb
+++ app/controllers/search_suggestions_controller.rb
@@ -1,6 +1,6 @@
 class SearchSuggestionsController < ApplicationController
   def index
     @project = Project.find(params[:project_id])
-    @search_suggestions = @project.search_suggestions
+    @search_suggestions = @project.search_suggestions.recent
   end
 end
--- app/models/search_suggestion.rb
+++ app/models/search_suggestion.rb
@@ -2,4 +2,6 @@ class SearchSuggestion < ApplicationRecord
   belongs_to :project
 
   validates :term, presence: true, uniqueness: { scope: :project_id, case_sensitive: false }
+
+  scope :recent, -> { order(created_at: :desc) }
 end
