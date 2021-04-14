36b5cfb45afc68f248cdd73d26fe0717367e2e99
abeidahmed <abeidahmed92@gmail.com>
2021-04-14 10:09:39 +0530

    Add cta

--- app/controllers/search_suggestions_controller.rb
+++ app/controllers/search_suggestions_controller.rb
@@ -1,4 +1,6 @@
 class SearchSuggestionsController < ApplicationController
+  skip_before_action :authenticate_user
+
   def index
     @project = Project.find(params[:project_id])
     @search_suggestions = @project.search_suggestions.recent
